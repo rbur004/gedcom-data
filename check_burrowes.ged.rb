7ec9739c8d93fc50531f9a25de53d7ea64b003bf
robertburrowes <r.burrowes@auckland.ac.nz>
2020-08-23 22:56:39 +1200

    Refactored to allow checks of other ged files

--- check_burrowes.ged.rb
+++ check_burrowes.ged.rb
@@ -1,14 +1,15 @@
 #!/usr/local/bin/ruby
 require 'gedcom'
 
-SOURCE="/Users/rbur004/Documents/Sync_Burrowes/Home/FamilyTree/gedcom_data/burrowes.ged"
-puts "parse burrowes.ged"
-g = Gedcom.file(SOURCE, "r:ASCII-8BIT") #OK with LF line endings.
-g.transmissions[0].summary
-puts
-puts "Self check of Gedcom consistency"
-g.transmissions[0].self_check #validate the gedcom file just loaded, printing errors found.
-puts 
+["burrowes.ged"].each do |source|
+  puts "parse #{source}"
+  g = Gedcom.file(__dir__ + '/' + source, "r:ASCII-8BIT") #OK with LF line endings.
+  g.transmissions[0].summary
+  puts
+  puts "Self check of Gedcom consistency"
+  g.transmissions[0].self_check #validate the gedcom file just loaded, printing errors found.
+  puts 
+end
 
 =begin
 #print the parsed file to see if it matches the source file.
